fc62b22d56657f23329260e59f73d5db717a7c2b
onlyrsh <theonlyrsh@gmail.com>
2023-11-17 16:07:06 +0500

    manager page and recieved application done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ApplicationController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Application;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('dashboard');
-})->middleware('auth');
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/', [MainController::class, 'main'])->name('main');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', [MainController::class, 'dashboard'])->name('dashboard');
+
+    Route::resource('applications', ApplicationController::class);
+
+});
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
